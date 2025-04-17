9ff05b1ff5750d4534e023a2285209e0e552f01c
Michael Buhler <michael@michaelbuhler.com>
2025-04-17 10:38:08 +0700

    chore: add spacing

--- seed/download-aos-module.sh
+++ seed/download-aos-module.sh
@@ -4,7 +4,7 @@ cd $(dirname $0)
 
 OUT_FILE=extras/aos.wasm
 
-if [ ! -f "$OUT_FILE"]; then
+if [ ! -f "$OUT_FILE" ]; then
     # TODO: need to find this module id programatically with a query to Arweave
     AOS_MODULE=JArYBF-D8q2OmZ4Mok00sD2Y_6SYEQ7Hjx-6VZ_jl3g
     echo "!!!"
